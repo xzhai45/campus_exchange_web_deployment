0914c0686a1b73e1f0b1dfb8574aac7862de448c
xzhai8 <xzhai8@emory.edu>
2025-03-01 23:31:59 -0500

    CSS working!

--- campus_exchange/urls.py
+++ campus_exchange/urls.py
@@ -28,5 +28,5 @@ urlpatterns = [
     
 ]
 
-urlpatterns += static(settings.MEDIA_URL,
-    document_root=settings.MEDIA_ROOT)
+if settings.DEBUG is False:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
