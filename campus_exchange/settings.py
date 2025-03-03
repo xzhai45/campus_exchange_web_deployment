b3044f084bacc0c8c128fa5f2e757a901e8d727e
xzhai8 <xzhai8@emory.edu>
2025-03-03 00:32:17 -0500

    Updated database settings

--- campus_exchange/settings.py
+++ campus_exchange/settings.py
@@ -83,10 +83,7 @@ WSGI_APPLICATION = "campus_exchange.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
 }
 
 
